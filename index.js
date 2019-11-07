024474f28d453c38adae5d5eabbd4f5c5f308478
Brandon Nichols <brandonaronnichols@gamil.com>
2019-11-07 01:15:24 -0600

    commented out server.get in index.js

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ server.listen(port, () => {
   console.log(`server listening on http://localhost:${port}/`);
 });
 
-server.get("/", (req, res) => {
-  req.status(200).send("You are at the index page");
-});
+// server.get("/", (req, res) => {
+//   req.status(200).send("You are at the index page");
+// });
 
 function logger(req, res, next) {
   const date = new Date();
